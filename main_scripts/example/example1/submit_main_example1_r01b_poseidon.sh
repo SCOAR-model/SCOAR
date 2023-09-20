5e751f9bf2904a0500c5be5a67c6e6a02932753e
Christoph Renkl <christoph.renkl@whoi.edu>
2023-09-20 16:52:02 -0400

    Update job submission scripts for example configuration

--- main_scripts/example/example1/submit_main_example1_r01b_poseidon.sh
+++ main_scripts/example/example1/submit_main_example1_r01b_poseidon.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
-#SBATCH -J r01b
-#SBATCH -t 12:00:00
-#SBATCH -o accounting/slurm-%j.out
-#SBATCH --ntasks=288
-#SBATCH -A s2634
-##SBATCH --qos=high
-##SBATCH --mail-user=user@nasa.gov
+#SBATCH --partition=compute
+#SBATCH --job-name=nep1_r01                   # Job name
+#SBATCH --mail-type=END                       # Mail events (BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=<your.email@whoi.edu>     # Where to send mail
+#SBATCH --ntasks=144                          # Number of MPI ranks
+#SBATCH --cpus-per-task=1                     # Number of cores per MPI rank
+#SBATCH --nodes=4                             # Number of nodes                        #
+#SBATCH --ntasks-per-node=36                  # How many tasks on each node
+#SBATCH --ntasks-per-socket=18                # How many tasks on each CPU or socket
+#SBATCH --distribution=cyclic:cyclic          # Distribute tasks cyclically on nodes & sockets
+#SBATCH --mem-per-cpu=4gb                     # Memory per processor
+#SBATCH --mem=150gb                           # Job Memory
+#SBATCH --time=24:00:00                       # Time limit hrs:min:sec
+#SBATCH --output=%j_slurm.log                 # Standard output/error
+#SBATCH --no-requeue
 
-export Couple_Run_Dir=$PROJECT/hseo4/SCOAR/Run/natl/natl1/r01
+scoar_dir=/vortexfs1/share/seolab/crenkl/models/SCOAR/main_scripts/example/example1
+Couple_Run_Dir=/vortexfs1/share/seolab/crenkl/models/SCOAR/Run/example/example1/r01
 read LastNHour < $Couple_Run_Dir/restart_info
 export RESTART=yes
 
-yyyye=2017
-mme=03
-dde=10
+yyyye=2014
+mme=11
+dde=21
 hhe=00
 
-./natl1_r01_restart.sh $yyyye:$mme:$dde:$hhe $RESTART $LastNHour >& r01_log_$$_$yyyye$mme$dde
+$scoar_dir/main_nep1_r01.sh $yyyye:$mme:$dde:$hhe $RESTART $LastNHour >& r01_log_$$_$yyyye$mme$dde$hhe
